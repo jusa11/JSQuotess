4f717c95fdf72d286c68d40d4f3b50ce992f08a2
jusa11 <a.juiceit@gmail.com>
2025-04-03 05:14:54 +0500

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   Navigate,
